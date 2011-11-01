9feafbd5b6e227871c40c2824043dbdee747ef9b
freethenation <klafter719r@yahoo.com>
2011-11-01 11:54:33 -0400

    1) Added canvas class and canvasOpenGL class

---
+++ JollyBit.Canvas.OpenGL/CanvasOpenGL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace JollyBit.TDS.Rendering.HTMLCanvas
+{
+	class CanvasOpenGL : Canvas
+	{
+		private Matrix4 _projMatrix;
+		public CanvasOpenGL()
+		{
+			_projMatrix = Matrix4.CreateOrthographicOffCenter(0, 1, 1, 0, -1, 1);
+		}
+
+		public override void BeginBatch()
+		{
+			GL.PushAttrib(AttribMask.AllAttribBits);
+			GL.Disable(EnableCap.DepthTest);
+
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.PushMatrix();
+			GL.LoadMatrix(ref _projMatrix);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.PushMatrix();
+			GL.LoadMatrix(ref Matrix4.Identity);
+
+			base.BeginBatch();
+		}
+
+		public override void EndBatch()
+		{
+			GL.PopAttrib();
+
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.PopMatrix();
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.PopMatrix();
+
+			base.EndBatch();
+		}
+
+		private void callInBatch(Action func)
+		{
+			bool inBatch = InBatch;
+			if (!inBatch) BeginBatch();
+			func();
+			if (!inBatch) EndBatch();
+		}
+
+		private void applyFillStyle()
+		{
+			GL.Color3(1, 1, 1);
+		}
+
+		protected override void fillConvexPolygon(IEnumerable<Vector3> points)
+		{
+			callInBatch(() =>
+			{
+				applyFillStyle();
+				GL.Begin(BeginMode.Polygon);
+				foreach (Vector3 point in points)
+				{
+					GL.Vertex3(point);
+				}
+				GL.End();
+			});
+		}
+	}
+}
